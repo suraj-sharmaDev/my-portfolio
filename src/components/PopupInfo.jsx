6213d653dc86a1c19a5630289df64ea4f86a97e8
Suraj Sharma <suraj.sharma@emvigotech.com>
2023-12-05 17:59:24 +0545

    Updated popup font size for smaller devices

--- src/components/PopupInfo.jsx
+++ src/components/PopupInfo.jsx
@@ -1,6 +1,6 @@
 const PopupInfo = () => {
   return (
-    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+    <h1 className="text-sm sm:leading-snug lg:text-xl text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
       Hi, I'm
       <span className="font-semibold mx-2 text-white">Suraj</span>
       👋
